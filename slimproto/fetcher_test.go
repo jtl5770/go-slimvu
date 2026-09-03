53592b1edf16a8124c25f95b1fb4ea06973eb9b0
Jens Lautenbacher <jtl@gmx.com>
2026-09-03 22:49:46 +0200

    chore: apply LGPL-3.0-or-later license and add headers to all source files

--- slimproto/fetcher_test.go
+++ slimproto/fetcher_test.go
@@ -1,3 +1,20 @@
+// Copyright (C) 2026 Jens Lautenbacher <jtl@gmx.com>
+//
+// This file is part of go-slimvu.
+//
+// go-slimvu is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-slimvu is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with go-slimvu.  If not, see <https://www.gnu.org/licenses/>.
+
 package slimproto
 
 import (
